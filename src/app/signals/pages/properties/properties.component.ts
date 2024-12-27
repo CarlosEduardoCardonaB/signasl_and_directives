635c0d09737526b1e989fa190fe5fa2ff2695f52
Carlos Eduardo <caeca425@gmail.com>
2024-12-27 16:47:55 -0500

    se finaliza clase de signals con el uso de effect en el properties.component.ts

--- src/app/signals/pages/properties/properties.component.ts
+++ src/app/signals/pages/properties/properties.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -9,6 +9,8 @@ import { User } from '../../interfaces/user-request.interface';
 })
 export class PropertiesComponent {
 
+  public counter = signal(10);
+
   public user = signal<User>({
     id: 2,
     email: "janet.weaver@reqres.in",
@@ -17,19 +19,41 @@ export class PropertiesComponent {
     avatar: "https://reqres.in/img/faces/2-image.jpg"
   });
 
-  public fullName = computed( () => {
+  // public fullName = computed( () => {
+  //   return `${this.user().first_name} ${this.user().last_name}`;
+  //   });
+
+  //Este funciona igual que el anterior, pero sin el return por lo que no usamos {}
+  public fullName = computed( () =>
     `${this.user().first_name} ${this.user().last_name}`
-  })
+  );
+
+  //Este dispara un efecto o alguna acción cuando una o varias señales cambian
+  public userChangedEffect = effect( () => {
+    //console.log('se disparó el effect')
+    //con la siguiente línea indicamos que cada vez que la signal "this.user()" cambia, entonces se dispara este effect e imprime el first_name por consola
+    //Este effect se limpia solo y no necesita el ngOnDestroy para ser limpiado manualmente
+    //console.log(this.user().first_name);
+
+    //Ejemplo de counter para demostración de auto limpieza
+    console.log(`${this.user().first_name} - ${this.counter()}`)
+  });
+
+  // Este boton es para probar que cuando volvemos a entrar al componente el contador se inicia desde cero, o sea que se limpia solo el signal
+  increaseBy( value: number ){
+    this.counter.update( () => this.counter() + value )
+  }
+
 
   //Con el keyof nos aseguramos que la llave que nos llega sea una que se encuentra en la interface "User" de lo contrario si lo manejamos como "field: string" nos pueden enviar campos que no existen en la interfaz y este los agreaga sin problema al objeto
   onFieldUpdated( field: keyof User, value: string ){
-    console.log({field, value});
+    //console.log({field, value});
 
     //Esta es una forma de hacerlo con el spread que son los 3 puntos "..." con estos solo actualizamos el campo que llega como "field" con el keyof
     // this.user.set({
     //   ...this.user(),
     //   [field]: value,
-    // })
+    // });
 
     //Otra fomra de hacerlo con el update
     // this.user.update( current => ({
@@ -39,28 +63,56 @@ export class PropertiesComponent {
 
 
     //De esta manera se agrega un poco de seguiridad para no ir a dañar el objeto de la interfaz
-    this.user.update( current => {
-      switch( field ){
+    // this.user.update( (current) => {
 
-          case 'email':
-            current.email = value;
-            break;
+    //   switch( field ){
 
-          case 'first_name':
-            current.first_name = value
-            break;
+    //       case 'email':
+    //         current.email = value;
+    //           break;
 
-          case 'last_name':
-            current.last_name = value
-            break;
+    //       case 'first_name':
+    //         current.first_name = value
+    //           break;
 
-          case 'avatar':
-            current.avatar = value
+    //       case 'last_name':
+    //         current.last_name = value
+    //           break;
+
+    //       case 'avatar':
+    //         current.avatar = value
+    //           break
+    //   }
+    //   return current;
+    // });
+
+
+    //Apunte de un estudiante del por que no sirve el switch anterior:
+    // Solo una aclaración, en el método onFieldUpdated usando el Switch final, no recalcula el FullName,
+    // porque estamos modificando directamente el current Value, mobx-state-tree (MST)
+    // no detecta el cambio cuando actualizamos directamente las propiedades del objeto observable.
+    // Lo que deberíamos hacer es retornar un nuevo objeto con los valores modificados.
+
+    this.user.update((current) => {
+
+      var newUser = {...current};
+      switch(field){
+        case 'email':
+          newUser.email = value;
+          break;
+        case 'first_name':
+          newUser.first_name = value;
+          break;
+        case 'last_name':
+          newUser.last_name = value;
+          break;
+        case 'avatar':
+          newUser.avatar = value
+           break
       }
-      return current;
+      return newUser;
+    });
 
-
-    })
 
   };
 }
